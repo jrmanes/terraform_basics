48e19e4867b1790a713ef9e3f58037fa7490bd70
Ubuntu <ubuntu@ip-172-31-14-203.eu-west-1.compute.internal>
2021-02-02 17:47:41 +0000

    add loop for volumes

---
+++ example_01_1/main.tf
@@ -0,0 +1,26 @@
+terraform {
+    required_providers {
+        docker = {
+            source = "kreuzwerker/docker"
+        }
+    }
+}
+
+provider "docker" {}
+
+resource "docker_image" "image-ubuntu" {
+    name = "${var.image_name}:${var.image_version}"
+}
+
+resource "docker_container" "ubuntu" {
+    name = "ubuntu_container"
+    image = docker_image.image-ubuntu.latest
+    
+    dynamic "volumes" {
+        for_each = var.volumes
+        content {
+            host_path      = volumes.value["host_path"]
+            container_path = volumes.value["container_path"]
+        }
+    }
+} 
---
+++ example_01_1/variables.tf
@@ -0,0 +1,26 @@
+variable "image_name" {
+    description = "Name of the image that we will use"
+    type = string
+    default = "ubuntu"
+}
+
+variable "image_version" {
+    description = "Version for the image that we will use"
+    type = string
+    default = "21.04"
+}
+
+variable "volumes" {
+    description = "All the volumes that we are going to mount inside container"
+    type = list(map(string))
+    default = [
+        {
+            host_path      = "/home/ubuntu/environment/terraform_basics/"
+            container_path = "/cursoTerraform"
+        },
+        {
+            host_path      = "/home/ubuntu/environment/teach"
+            container_path = "/teach/"
+        }
+    ]
+}
