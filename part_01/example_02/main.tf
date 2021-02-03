8ea43200211efbaaac6c96afad0f8f948d28116f
Ubuntu <ubuntu@ip-172-31-14-203.eu-west-1.compute.internal>
2021-02-03 17:46:39 +0000

    loops differents

--- part_01/example_02/main.tf
+++ part_01/example_02/main.tf
@@ -13,6 +13,15 @@ resource "docker_image" "image-nginx" {
 }
 
 resource "docker_container" "nginx" {
+    name  = "nginx_container"
+    image = docker_image.image-nginx.latest
+    ports {
+        internal = 80
+        external = 8088
+    }
+}
+
+resource "docker_container" "multiple_nginx" {
     count = 3
     name  = "nginx_container_${count.index}"
     image = docker_image.image-nginx.latest
@@ -21,3 +30,33 @@ resource "docker_container" "nginx" {
         external = "808${count.index}"
     }
 } 
+
+resource "docker_container" "envs_nginx" {
+    for_each = var.nginx_config
+
+    name = each.key
+    image = docker_image.image-nginx.latest
+    ports {
+        internal = each.value["ports_internal"]
+        external = each.value["ports_external"]
+    }
+    volumes {
+        host_path      = each.value["host_path"]
+        container_path = each.value["container_path"]
+    }
+}
+
+resource "docker_container" "envs_nginx_pro" {
+    count = length(var.nginx_config_pro)
+
+    name = var.nginx_config_pro[count.index]["name"]
+    image = docker_image.image-nginx.latest
+    ports {
+        internal = var.nginx_config_pro[count.index]["ports_internal"]
+        external = var.nginx_config_pro[count.index]["ports_external"]
+    }
+    volumes {
+        host_path      = var.nginx_config_pro[count.index]["host_path"]
+        container_path = var.nginx_config_pro[count.index]["container_path"]
+    }
+}
--- part_01/example_02/variables.tf
+++ part_01/example_02/variables.tf
@@ -9,3 +9,68 @@ variable "image_version" {
     type = string
     default = "1.19.6"
 }
+
+variable "container_names" {
+    description = "Containers nginx"
+    type = map(string)
+    default = {
+        contenedorA = "8090"
+        contenedorB = "8091"
+    }
+}
+
+variable "nginx_config" {
+    description = "All the volumes that we are going to mount inside container"
+    type = map(map(string))
+    default = {
+        nginx_1 = {
+            name= "nginx_DEV"
+            ports_internal  = "80"
+            ports_external  = "8085"
+            container_path = "/test"
+            host_path      = "/home/ubuntu/environment/"
+        }
+        nginx_2 = {
+            name            = "nginx_PRE"
+            ports_internal  = "80"
+            ports_external  = "7998"
+            container_path = "/test"
+            host_path      = "/home/ubuntu/environment/"
+        }
+        nginx_3 = {
+            name             = "nginx_PRO"
+            ports_internal   = "80"
+            ports_external   = "7999"
+            container_path = "/test"
+            host_path      = "/home/ubuntu/environment/"
+        }
+    }
+}
+
+variable "nginx_config_pro" {
+    description = "All the volumes that we are going to mount inside container"
+    type = list(map(string))
+    default = [
+        {
+            name            = "nginx_DEV"
+            ports_internal  = "80"
+            ports_external  = "8095"
+            container_path  = "/test"
+            host_path       = "/home/ubuntu/environment/"
+        },
+        {
+            name            = "nginx_PRE"
+            ports_internal  = "80"
+            ports_external  = "7988"
+            container_path  = "/test"
+            host_path       = "/home/ubuntu/environment/"
+        },
+        {
+            name             = "nginx_PRO"
+            ports_internal   = "80"
+            ports_external   = "7979"
+            container_path   = "/test"
+            host_path        = "/home/ubuntu/environment/"
+        }
+    ]
+}
