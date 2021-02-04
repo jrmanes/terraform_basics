86e737234422582751acb96d6a1782fb6f4fb809
Ubuntu <ubuntu@ip-172-31-14-203.eu-west-1.compute.internal>
2021-02-04 18:31:50 +0000

    part5 created - aws instance

---
+++ part_04_ec2/main.tf
@@ -0,0 +1,17 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+        }
+    }
+}
+
+provider {
+    profile = "default"
+    region = "eu-west-1"
+}
+
+resource "aws_instance" "ec2" {
+    ami = "ami-0aef57767f5404a3c"
+    instance_type = "t2.micro"
+}
